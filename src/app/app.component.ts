9194f3d534bc72258f795f575cbd1509e5baa6e0
Kaue <vetorazikaue@hotmail.com>
2024-03-26 15:26:03 -0300

    testes diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet,
+    FirstComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'curso-regular';
+
+  userName:string = "Joaquim";
+  userSurname:string = "Silva";
+  userAge:number = 31;
+  userLogin = {
+    login : 'kaue',
+    senha: 'vetorazikaue@hotmail.com'
+  }
 }
