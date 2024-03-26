7e8906c8d79e0c2d638a1932015f1179f37cda5b
Kaue <vetorazikaue@hotmail.com>
2024-03-26 16:18:58 -0300

    eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,9 @@ import { DirectivesComponent } from './components/directives/directives.componen
   imports: [RouterOutlet,
     FirstComponentComponent,
     ParentDataComponent,
-    DirectivesComponent],
+    DirectivesComponent,
+    IfRenderComponent,
+    EventosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
